8731a4fe515b3598053d3db7a988227001be9279
Saad <Saadisone7@gmail.com>
2018-09-21 20:48:05 +0930

    Writing a function to display current day and time

--- js/dailyalerts.js
+++ js/dailyalerts.js
@@ -5,4 +5,42 @@ document.addEventListener('DOMContentLoaded', () => {
   const ul = document.querySelector('.main ul');
   const input = form.querySelector('input');
   const clearButton = mainForm.querySelector('#clear-button');
-  
+
+  // Function to display time on screen
+   const displayClock = () => {
+     const timeDate = new Date();
+     const clock = document.querySelector("#clock");
+     const date = document.querySelector('#date');
+
+     let hours = timeDate.getHours() % 12;
+     let minutes = timeDate.getMinutes();
+     let dayOrNight = '';
+
+     if (hours.toString().length < 2) {
+       hours = '0' + hours;
+     }
+
+     if (minutes.toString().length < 2) {
+       minutes = '0' + minutes;
+     }
+
+     if (timeDate.getHours() <= 12) {
+       dayOrNight = 'AM';
+     } else {
+       dayOrNight = 'PM';
+     }
+
+     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+     const today = days[timeDate.getDay()];
+
+     const clockOutput = hours + ' : ' + minutes + ' ' + dayOrNight;
+     const dateOutput = 'Add task for ' + today;
+
+     clock.textContent = clockOutput;
+     date.textContent = dateOutput;
+   }
+
+   window.onload = clockFunction = () => {
+     displayClock();
+     setInterval(displayClock, 1000)
+   }
