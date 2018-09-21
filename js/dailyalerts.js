a1ed80ef16706d8d9a5b4da75e08ac80afaa289d
Saad <Saadisone7@gmail.com>
2018-09-21 21:19:38 +0930

    Changed ID to Class
    For best CSS practices.

--- js/dailyalerts.js
+++ js/dailyalerts.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const mainForm = document.querySelector('#main-form');
+  const mainForm = document.querySelector('.main-form');
   const form = document.querySelector('form');
   const submitButton = form.querySelector('button');
   const ul = document.querySelector('.main ul');
   const input = form.querySelector('input');
-  const clearButton = mainForm.querySelector('#clear-button');
+  const clearButton = mainForm.querySelector('.clear-button');
 
   // Function to display time on screen
    const displayClock = () => {
      const timeDate = new Date();
-     const clock = document.querySelector("#clock");
-     const date = document.querySelector('#date');
+     const clock = document.querySelector(".clock");
+     const date = document.querySelector('.date');
 
      let hours = timeDate.getHours() % 12;
      let minutes = timeDate.getMinutes();
