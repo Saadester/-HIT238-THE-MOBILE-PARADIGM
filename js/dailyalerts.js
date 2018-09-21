c315cd838afa60a03d5c8be8409ea6243863d669
Saad <Saadisone7@gmail.com>
2018-09-21 20:47:28 +0930

    Added Event Listener for buttons

---
+++ js/dailyalerts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const mainForm = document.querySelector('#main-form');
+  const form = document.querySelector('form');
+  const submitButton = form.querySelector('button');
+  const ul = document.querySelector('.main ul');
+  const input = form.querySelector('input');
+  const clearButton = mainForm.querySelector('#clear-button');
+  
