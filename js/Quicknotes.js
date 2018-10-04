e87514d07f8944f59562f445b8c0ec561d02bfb7
Saad <Saadisone7@gmail.com>
2018-10-05 02:47:48 +0930

    Buttton and its function to add notes

--- js/Quicknotes.js
+++ js/Quicknotes.js
@@ -31,76 +31,97 @@
 		    });
 		},
 
-      // Adding the time and date on top of notes ( the time and date note was created )
+					// Adding the time and date on top of notes ( the time and date note was created )
 
-    addNewNote: function(className, title, content, time) {
-  if (!className) {
-      className = "c-" + Math.ceil(Math.random() * 3);
-      if(!time) {
-        var date = new Date();
-        var time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  (date.getHours() - 4) + ":" + date.getMinutes() + ":" + date.getSeconds();
-      }
-    }
-    //Setting up local storage for save,delete and add notes
+        addNewNote: function(className, title, content, time) {
+			if (!className) {
+					className = "c-" + Math.ceil(Math.random() * 3);
+					if(!time) {
+						var date = new Date();
+						var time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  (date.getHours() - 4) + ":" + date.getMinutes() + ":" + date.getSeconds();
+					}
+				}
+									//Setting up local storage for save,delete and add notes
 
 
-      notes.append("<li><div class='" + className + "'><i class='icon-check'>  Save</i><p class='time'>" + time + "</p>" + "<textarea class='note-title' placeholder='Enter a title...' maxlength='50'/>" + "<textarea class='note-content' placeholder='Start a note...'/>" + "<i class='icon-cancel hide'> Delete</i>" + "</div></li>");
+				notes.append("<li><div class='" + className + "'><i class='icon-check'>  Save</i><p class='time'>" + time + "</p>" + "<textarea class='note-title' placeholder='Enter a title...' maxlength='50'/>" + "<textarea class='note-content' placeholder='Start a note...'/>" + "<i class='icon-cancel hide'> Delete</i>" + "</div></li>");
 
-      var newNote = notes.find("li:last");
-      newNote.find(".icon-cancel").on('click', function(){
-          newNote.remove();
-          _private.saveNotes();
-      });
-      newNote.find(".icon-check").on('click', function(){
-        _private.saveNotes();
-        $("body").fadeIn(175).fadeOut(175).fadeIn(175)
-      })
+				var newNote = notes.find("li:last");
+				newNote.find(".icon-cancel").on('click', function(){
+				    newNote.remove();
+				    _private.saveNotes();
+				});
+				newNote.find(".icon-check").on('click', function(){
+					_private.saveNotes();
+					$("body").fadeIn(175).fadeOut(175).fadeIn(175)
+				})
 
-      _private.attachNoteEvent(newNote);
+				_private.attachNoteEvent(newNote);
 
-      if (title) {
-        newNote.find("textarea.note-title").val(title);
-        }
+				if (title) {
+					newNote.find("textarea.note-title").val(title);
+			    }
 
-      if (content) {
-        newNote.find("textarea.note-content").val(content);
-        }
-        if (time){
-          newNote.find(".time").text();
-        }
+				if (content) {
+					newNote.find("textarea.note-content").val(content);
+			    }
+			    if (time){
+			    	newNote.find(".time").text();
+			    }
 
-        _private.saveNotes();
-      },
-   saveNotes: function() {
-      var notesArray = [];
+			    _private.saveNotes();
+        },
+		 saveNotes: function() {
+		    var notesArray = [];
 
-      notes.find("li > div").each(function (i, e) {
-          var c = $(e).attr("class");
-          var title = $(e).find("textarea.note-title");
-          var content = $(e).find("textarea.note-content");
-          var time = $(e).find('.time').text();
+		    notes.find("li > div").each(function (i, e) {
+		        var c = $(e).attr("class");
+		        var title = $(e).find("textarea.note-title");
+		        var content = $(e).find("textarea.note-content");
+		        var time = $(e).find('.time').text();
 
-          notesArray.push({ Index: i, Title: title.val(), Content: content.val(), Class: c, Time: time });
-      });
-      // If notes more than 0, Function load notes from local storage
+		        notesArray.push({ Index: i, Title: title.val(), Content: content.val(), Class: c, Time: time });
+		    });
 
-      		    var jsonStr = JSON.stringify(notesArray);
+						// If notes more than 0, Function load notes from local storage
 
-      		    localStorage.setItem("notes", jsonStr);
-      		},
-              loadNotes: function() {
-      		    var localNotes = localStorage.getItem("notes");
-      		    if ( localNotes ) {
-      		        var notesList = JSON.parse(localNotes);
-      		  		count = notesList.length;
-      		        var i;
-      		        for (i = 0; i < count; i++) {
-      		            var storedNote = notesList[i];
-      		            _private.addNewNote(storedNote.Class, storedNote.Title, storedNote.Content, storedNote.Time);
-      		        }
-      		        $('#controls strong').hide();
-      		        $('#welcome').html('Quick Notes')
-      		    }
-      		}
-          };
-      						
+		    var jsonStr = JSON.stringify(notesArray);
+
+		    localStorage.setItem("notes", jsonStr);
+		},
+        loadNotes: function() {
+		    var localNotes = localStorage.getItem("notes");
+		    if ( localNotes ) {
+		        var notesList = JSON.parse(localNotes);
+		  		count = notesList.length;
+		        var i;
+		        for (i = 0; i < count; i++) {
+		            var storedNote = notesList[i];
+		            _private.addNewNote(storedNote.Class, storedNote.Title, storedNote.Content, storedNote.Time);
+		        }
+		        $('#controls strong').hide();
+		        $('#welcome').html('Quick Notes')
+		    }
+		}
+    };
+
+					// linking button with css to add notes
+
+    $(document).ready(function() {
+    	//Begins here
+    	notes = $('#notes-list');
+    	_private.loadNotes();
+
+	    $("#btnNew").click(function () {
+	    	var t = 1;
+	        _private.addNewNote();
+	    });
+	    if (count === 0) {
+	    	 $('#welcome').html('Welcome to quick notes!')
+	    	 $('#controls strong').show();
+	    }
+
+    });
+
+    return {}
+})();
