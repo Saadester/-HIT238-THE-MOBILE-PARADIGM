026cb17e52dd53db5c9dcaafe27fc44263bf76be
Saad <Saadisone7@gmail.com>
2018-10-05 17:55:40 +0930

    add note to the document

--- js/Quicknote.js
+++ js/Quicknote.js
@@ -4,8 +4,18 @@ var savedNotes = localStorage.getItem('noteAppStorageObj');
 
 // Does the object retrieved from storage contain any information?
 // If so do this
-
 if (savedNotes !== null) {
+
   savedNotes = JSON.parse(savedNotes);
+
   savedNotes = Object.keys(savedNotes).map(function (key) { return savedNotes[key]; });
-  
+
+  for(var x = 0; x < savedNotes.length; x++){
+
+    // This takes the contents of the note and wraps it in a .note div for formatting
+    var newNote = '<div class="note">' + savedNotes[x] + '</div>';
+
+    // This adds the note to the document
+    document.querySelector("body").innerHTML += newNote;
+  }
+}
