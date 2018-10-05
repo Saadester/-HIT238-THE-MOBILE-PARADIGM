7f7f976f9aa27c7eeea68a0432c716f7d86c39c5
Saad <Saadisone7@gmail.com>
2018-10-05 18:07:56 +0930

    new note add and remove and function to remove note screen

--- js/Quicknote.js
+++ js/Quicknote.js
@@ -26,7 +26,7 @@ function addNoteScreen() {
 
   // Animate the plus new note button to turn into an X with the twist class
   document.querySelector("#add-note-btn").classList.add("twist");
-
+  // Assigning buttons
   elString += '<div id="add-note-screen">';
   elString += '<h1>Add New Note</h1>';
   elString += '<textarea id="new-note-content" autofocus></textarea>';
@@ -50,3 +50,37 @@ function addNoteScreen() {
   document.querySelector("#insert-questionmark").addEventListener("click", function(){
     typeInTextarea(document.querySelector("textarea"), "?")
   }, false);
+
+  // Eventlistner for Submit new note button nad remove buttons
+  document.querySelector("#submit-new-note").addEventListener("click", addNoteToList, false);
+}
+
+function removeNoteScreen() {
+
+  // Turns the X back into a plus by removing the twist class
+  document.querySelector("#add-note-btn").classList.remove("twist");
+
+  // Add class with animation to add-note-screen so animates out
+  document.querySelector("#add-note-screen").classList.add("note-screen-out");
+
+  // Wait half a second to remove the add-note-screen so animation can play
+  setTimeout(function(){
+
+  // removes the add-note-screen from the document
+  document.querySelector("body").removeChild(document.querySelector("#add-note-screen"));
+  }, 500);
+
+  // This switches and event listener attached to the add-note-btn to create the note dialog instead of closing it
+  document.querySelector("#add-note-btn").removeEventListener("click", removeNoteScreen);
+  document.querySelector("#add-note-btn").addEventListener("click", addNoteScreen, false);
+
+  delNoteBtns();
+}
+  //This function is triggered by clicking the add-note-btn in the create note window
+function addNoteToList() {
+
+  var noteText = document.querySelector("#new-note-content").value;
+
+  if(noteText === "" || noteText === " "){
+      return false;
+  }
