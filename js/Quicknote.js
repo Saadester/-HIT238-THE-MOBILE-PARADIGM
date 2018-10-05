d5fda12006cef261589ccc3f3ca51a21b8ca6ffb
Saad <Saadisone7@gmail.com>
2018-10-05 17:54:28 +0930

    the object from the previous line and turns it into an array

--- js/Quicknote.js
+++ js/Quicknote.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 var savedNotes = localStorage.getItem('noteAppStorageObj');
+
+// Does the object retrieved from storage contain any information?
+// If so do this
+
+if (savedNotes !== null) {
+  savedNotes = JSON.parse(savedNotes);
+  savedNotes = Object.keys(savedNotes).map(function (key) { return savedNotes[key]; });
+  
