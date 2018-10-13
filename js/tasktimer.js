c7f707e4e511e76257bb3b967e0f20605d9d13b7
Saad <Saadisone7@gmail.com>
2018-10-14 02:12:54 +0930

    Added project class ( and ID) and gave it return function

--- js/tasktimer.js
+++ js/tasktimer.js
@@ -1,1 +1,31 @@
-## working on Javascript at backend ( time to sleep now lol)
+const dataController = (function() {
+
+    class Project {
+        constructor(id, title) {
+            this.id = id;
+            this.title = title;
+        }
+    }
+
+    const projects = {
+        allProjects: []
+    };
+
+    return {
+
+        addProject: function(title) {
+
+            let ID;
+            if (projects.allProjects.length > 0) {
+                ID = projects.allProjects[projects.allProjects.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            const newProject = new Project(ID, title);
+                // Add the project to the project data
+            projects.allProjects.push(newProject);
+
+            return newProject;
+
+        },
